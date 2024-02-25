fd5f7057d0941fe4dd261112ba3e125569283ef6
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2024-02-26 02:51:12 +0530

    made change in text document

--- src/TextDocument.js
+++ src/TextDocument.js
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import io from "socket.io-client";
 import "./TextDocument.css";
+import { useAuth } from "./AuthContext"; // Import useAuth hook
 
 const TextDocument = () => {
   const { id: roomId } = useParams();
   const [text, setText] = useState("");
   const [socket, setSocket] = useState(null);
+  const { authenticated } = useAuth(); // Get authenticated user information
 
   useEffect(() => {
     const socketIo = io("http://localhost:3003");
 
-    socketIo.emit("joinRoom", roomId); // Join the room for the document ID
+    socketIo.emit("joinRoom", {
+      roomId,
+      userId: localStorage.getItem("userId"),
+    }); // Pass user ID to the server
 
     socketIo.on("updateText", (data) => {
       setText(data);
@@ -27,7 +32,11 @@ const TextDocument = () => {
   const handleTextChange = (event) => {
     const newText = event.target.value;
     setText(newText);
-    socket.emit("textChange", { roomId, newText });
+    socket.emit("textChange", {
+      roomId,
+      newText,
+      userId: localStorage.getItem("userId"),
+    }); // Pass user ID to the server
   };
 
   return (
@@ -37,6 +46,7 @@ const TextDocument = () => {
         className="custom-textarea"
         value={text}
         onChange={handleTextChange}
+        disabled={!authenticated} // Disable textarea if user is not authenticated
       />
     </div>
   );
